6ae7d6ebafa50dcbc4a4c5b6a94070e2d8b69507
Leon Tan <101375873+itsLeonTan@users.noreply.github.com>
2026-02-21 11:27:25 -0800

    V1.1.0 - Added colours
    - Each particle has a randomized colour.
    - Particle colour change when they make contact with the borders.
    - The colour of each link is a mix of both particles it connects.
    - The lightness of each link is influenced by the distance between the two particles.

--- Particle.java
+++ Particle.java
@@ -1,11 +1,28 @@
 public class Particle {
     public int x, y, r;
     public float dir; // Direction of travel
+    public float hue; 
 
     public Particle() {
         r = 3;
         x = (int) (Math.random() * (ParticleField.WIDTH - 2 * r));
         y = (int) (Math.random() * (ParticleField.HEIGHT - 2 * r));
         dir = (float) (Math.random() * 360);
+        hue = (float) Math.random();
+    }
+
+    public void changeHue() {
+        hue = (float) Math.random();
+    }
+
+    public float mixHue(float hue2) {
+        float dif = hue2 - hue; // Raw difference
+        
+        // shift the range to -180 to 180 to find the shortest path
+        dif = ((dif + 180f) % 360f); 
+        if (dif < 0) dif += 360f;
+        dif -= 180f;
+
+        return (hue + dif / 2f + 360f) % 360f; // 
     }
 }
--- ParticlePanel.java
+++ ParticlePanel.java
@@ -18,15 +18,15 @@ public class ParticlePanel extends JPanel {
     }
 
     private void update(ActionEvent e) {
-
-
         double step = 3.0;
         for (Particle p:particles) {
             if (p.x + p.r * 2 > ParticleField.WIDTH || p.x < 0) {
                 p.dir = (float) ((360.0 - p.dir) % 360.0);
+                p.changeHue();
             }
             if (p.y + p.r * 2 > ParticleField.HEIGHT || p.y < 0) {
                 p.dir = (float) ((180.0 - p.dir + 360.0) % 360.0);
+                p.changeHue();
             }
 
             double rad = Math.toRadians(p.dir);
@@ -42,18 +42,22 @@ public class ParticlePanel extends JPanel {
         super.paintComponent(g); // Clear screen
         setBackground(Color.BLACK);
         g.setColor(Color.WHITE);
+        
+        final int limit = 200;
 
         for (int i = 0; i < particles.length; i++) {
-            g.fillOval(particles[i].x, particles[i].y, particles[i].r * 2, particles[i].r * 2);
-
             for (int j = i + 1; j < particles.length; j++) {
                 float d = (float) Math.sqrt(Math.pow(particles[j].x - particles[i].x, 2) + Math.pow(particles[j].y - particles[i].y, 2));
 
-                if (d < 200) {
+                if (d < limit) {
+                    g.setColor(Color.getHSBColor(particles[i].mixHue((float) particles[j].hue), 1.0f, 1f - d / limit));
                     int r = particles[i].r;
                     g.drawLine(particles[i].x + r, particles[i].y + r, particles[j].x + r, particles[j].y + r);
                 }
             }
+
+            g.setColor(Color.getHSBColor(particles[i].hue, 1.0f, 0.8f));
+            g.fillOval(particles[i].x, particles[i].y, particles[i].r * 2, particles[i].r * 2);
         }
     }
 }
